ef536f78ec3d1fe26a71bcc3abad18286cf3400e
croovr <croovr.ng@gmail.com>
2025-11-27 12:02:44 +0100

    Improved Prisma client initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,10 +4,12 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["query"],
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query"] : [],
   });
+};
+
+export const prisma = global.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
